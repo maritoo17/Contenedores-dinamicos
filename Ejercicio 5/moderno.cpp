9321e4eda908ded3dc5338bba066180a05235bd9
maritoo17 <msancgim@myuax.com>
2023-11-29 20:13:50 +0100

    Final del ejercicio en el que hago una lista con numeros del 1 al 5 en el que pongo en un principio los números originales y luego hago que esos mismos numeros se eleven al cuadrado.

--- Ejercicio 5/moderno.cpp
+++ Ejercicio 5/moderno.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
 #include <vector>
-#include <memory>
 #include <algorithm>
 
 using namespace std;
 
+int main() {
+    vector<int> numeros;
+    numeros.push_back(1);
+    numeros.push_back(2);
+    numeros.push_back(3);
+    numeros.push_back(4);
+    numeros.push_back(5);
 
+    cout << "Números originales:" << endl;
+    for (auto num : numeros) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    vector<int> numerosCuadrados;
+    numerosCuadrados.reserve(numeros.size());
+
+    for (auto num : numeros) {
+        numerosCuadrados.push_back(num * num);
+    }
+
+    cout << "Cuadrados de los números: " << endl;
+    for (auto num : numerosCuadrados) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
