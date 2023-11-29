3da67cbde316ebd4cb62783b845e3f8343a26633
maritoo17 <msancgim@myuax.com>
2023-11-29 17:30:12 +0100

    Tercera parte del ejercicio en el que he añadido un namespace std para acortar el código y he modificado la lista de tal forma que no me salte un error. La última parte del código, se encargará de ordenar los valores de menor a mayor a la hora de imprimirlos.

--- Ejercicio 3/algoritmos.cpp
+++ Ejercicio 3/algoritmos.cpp
@@ -2,4 +2,16 @@
 #include <vector>
 #include <algorithm>
 
-std::vector<int> numeros = {7, 2, 5, 1, 9, 3};
+using namespace std;
+
+int main(){
+    vector<int> numeros;
+    numeros.push_back(7);
+    numeros.push_back(3);
+    numeros.push_back(5);
+    numeros.push_back(1);
+    numeros.push_back(9);
+
+    sort(numeros.begin(), numeros.end());
+
+}
