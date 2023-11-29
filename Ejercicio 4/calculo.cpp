d20203922cf15d8d2728bbbb4b672f3ed1a45ee8
maritoo17 <msancgim@myuax.com>
2023-11-29 18:56:05 +0100

    Cuarta parte en la que añado los resultados del código pero me sale un error y por lo tanto no salen las operaciones si no que sale un error.

--- Ejercicio 4/calculo.cpp
+++ Ejercicio 4/calculo.cpp
@@ -10,9 +10,33 @@ int main (){
     numeros[2]= 3.1;
     numeros[3]= 4.9;
 
-    valarray<double> suma = numeros + valarray<double>(2.0);
-    valarray<double> resta = numeros - valarray<double>(1.0);
-    valarray<double> multiplicacion = numeros * valarray<double>(3.0);
-    valarray<double> division = numeros / valarray<double>(2.0);
+    valarray<double> suma = numeros + 2.0;
+    valarray<double> resta = numeros - 1.0;
+    valarray<double> multiplicacion = numeros * 3.0;
+    valarray<double> division = numeros / 2.0;
 
+    std::cout << "Valores originales: ";
+    for (double num : numeros) {
+        std::cout << num << " ";
+    }
+
+    std::cout << "\nSuma: ";
+    for (double num : suma) {
+        std::cout << num << " ";
+    }
+    std::cout << "\nResta: ";
+    for (double num : resta) {
+        std::cout << num << " ";
+    }
+    std::cout << "\nMultiplicacion: ";
+    for (double num : multiplicacion) {
+        std::cout << num << " ";
+    }
+    std::cout << "\nDivision: ";
+    for (double num : division) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
 }
