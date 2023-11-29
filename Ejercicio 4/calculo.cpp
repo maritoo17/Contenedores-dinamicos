09f756ed350490047c5ede1ef2be12b6c41cd1a6
maritoo17 <msancgim@myuax.com>
2023-11-29 17:55:38 +0100

    Tercera parte del ejercicio en la que añado las operaciones y corrijo la forma de la lista para que no me salga un error.

--- Ejercicio 4/calculo.cpp
+++ Ejercicio 4/calculo.cpp
@@ -4,6 +4,15 @@
 using namespace std;
 
 int main (){
-    valarray<double> numeros = {1.7, 2.5, 3.1, 4.9};
+    valarray<double> numeros;
+    numeros[0]= 1.7;
+    numeros[1]= 2.5;
+    numeros[2]= 3.1;
+    numeros[3]= 4.9;
+
+    valarray<double> suma = numeros + valarray<double>(2.0);
+    valarray<double> resta = numeros - valarray<double>(1.0);
+    valarray<double> multiplicacion = numeros * valarray<double>(3.0);
+    valarray<double> division = numeros / valarray<double>(2.0);
 
 }
